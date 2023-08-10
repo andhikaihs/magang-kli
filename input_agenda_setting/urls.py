741d0be392af692d1a2f05d9a3fc4391fdec3dfb
Najwa Kariza <9g.najwakariza@gmail.com>
2023-08-10 15:37:14 +0700

    update

--- input_agenda_setting/urls.py
+++ input_agenda_setting/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('input-agenda-setting/', views.input_as, name='input-as'),
+    path('data-input-agenda-setting/', views.table_input_as, name='table_input_as'),
+    path('edit-agenda-setting/<int:id>', views.edit_agenda_setting, name='edit_agenda_setting')
 ]
--- input_agenda_setting/views.py
+++ input_agenda_setting/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import InputAgendaSetting
+from django.shortcuts import render, get_object_or_404, redirect
+from input_agenda_setting.models import InputAgendaSetting
 
 def input_as(request):
     if request.method == 'POST':
@@ -25,10 +25,31 @@ def input_as(request):
 
 def table_input_as(request):
     input_as_data = InputAgendaSetting.objects.all()
-    total_input_as = InputAgendaSetting.count()
+    total_input_as = InputAgendaSetting.objects.count()
     
     context = {
         'input_as_data': input_as_data,
-        'total_input_as': total_input_as,
+        'count_total_input_as': total_input_as,
     }
-    return render(request, 'table_input_as.html', context)
+    return render(request, 'table_input-as.html', context)
+
+def edit_agenda_setting(request, id):
+    agenda_setting = get_object_or_404(InputAgendaSetting, id=id)
+    if request.method == 'POST':
+        nomor_agenda = request.POST.get('nomor_agenda')
+        topik_agenda = request.POST.get('topik_agenda')
+        pesan_kunci = request.POST.get('pesan_kunci')
+        sub_pesan_kunci = request.POST.get('sub_pesan_kunci')
+        agenda_date_time_start = request.POST.get('agenda_date_time_start')
+        agenda_date_time_end = request.POST.get('agenda_date_time_end')
+        
+        agenda_setting.nomor_agenda = nomor_agenda
+        agenda_setting.topik_agenda = topik_agenda
+        agenda_setting.pesan_kunci = pesan_kunci
+        agenda_setting.sub_pesan_kunci = sub_pesan_kunci
+        agenda_setting.agenda_date_time_start = agenda_date_time_start
+        agenda_setting.agenda_date_time_end = agenda_date_time_end
+        agenda_setting.save()
+        
+        return redirect('table_input_as')
+    return render(request, 'edit_input-as.html', {'agenda_setting': agenda_setting})
