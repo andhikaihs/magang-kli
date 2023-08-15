b3e83be4cc28904deeabd4e18ed6044b734e8fbb
Najwa Kariza <9g.najwakariza@gmail.com>
2023-08-15 20:12:31 +0700

    refactor: delete input-as then change it into edit as

--- input_agenda_setting/urls.py
+++ input_agenda_setting/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('input-agenda-setting/', views.input_as, name='input-as'),
     path('data-input-agenda-setting/', views.table_input_as, name='table_input_as'),
     path('edit-agenda-setting/<int:id>', views.edit_agenda_setting, name='edit_agenda_setting'),
     path('delete/<int:nomor_agenda>/', views.delete_agenda_setting, name='delete_agenda_setting'),
--- input_agenda_setting/views.py
+++ input_agenda_setting/views.py
@@ -1,28 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from input_agenda_setting.models import InputAgendaSetting
 
-def input_as(request):
-    if request.method == 'POST':
-        nomor_agenda = request.POST.get('nomor_agenda')
-        topik_agenda = request.POST.get('topik_agenda')
-        pesan_kunci = request.POST.get('pesan_kunci')
-        sub_pesan_kunci = request.POST.get('sub_pesan_kunci')
-        
-        # Ambil rentang tanggal dan waktu dari form
-        rentang_tanggal_waktu = request.POST.get('agenda_date_time')
-        tanggal_awal, tanggal_akhir = rentang_tanggal_waktu.split(' to ')
-
-        InputAgendaSetting.objects.create(
-            nomor_agenda=nomor_agenda,
-            topik_agenda=topik_agenda,
-            pesan_kunci=pesan_kunci,
-            sub_pesan_kunci=sub_pesan_kunci,
-            agenda_date_time_start=tanggal_awal,
-            agenda_date_time_end=tanggal_akhir, 
-        )
-
-    return render(request, 'input-as.html')
-
 def table_input_as(request):
     input_as_data = InputAgendaSetting.objects.all()
     total_input_as = InputAgendaSetting.objects.count()
@@ -40,17 +18,18 @@ def edit_agenda_setting(request, id):
         topik_agenda = request.POST.get('topik_agenda')
         pesan_kunci = request.POST.get('pesan_kunci')
         sub_pesan_kunci = request.POST.get('sub_pesan_kunci')
-        agenda_date_time_start = request.POST.get('agenda_date_time_start')
-        agenda_date_time_end = request.POST.get('agenda_date_time_end')
         
+        rentang_tanggal_waktu = request.POST.get('agenda_date_time')
+        tanggal_awal, tanggal_akhir = rentang_tanggal_waktu.split(' to ')
+
         agenda_setting.nomor_agenda = nomor_agenda
         agenda_setting.topik_agenda = topik_agenda
         agenda_setting.pesan_kunci = pesan_kunci
         agenda_setting.sub_pesan_kunci = sub_pesan_kunci
-        agenda_setting.agenda_date_time_start = agenda_date_time_start
-        agenda_setting.agenda_date_time_end = agenda_date_time_end
+        agenda_setting.agenda_date_time_start = tanggal_awal
+        agenda_setting.agenda_date_time_end = tanggal_akhir
         agenda_setting.save()
-        
+
         return redirect('table_input_as')
     return render(request, 'edit_input-as.html', {'agenda_setting': agenda_setting})
 
