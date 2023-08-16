1603bfc38326116d26e916a2a8d53e2baac08c2c
Najwa Kariza <9g.najwakariza@gmail.com>
2023-08-16 11:35:42 +0700

    refactor: change some infos

--- socmed_data/urls.py
+++ socmed_data/urls.py
@@ -9,9 +9,5 @@ urlpatterns = [
     # path('social-media/instagram/<str:ue1>/', views.instagram_detail_webscrap, name='instagram_detail_webscrap'),
 
     path('social-medial/linkedin/', views.fetch_linkedin_data, name='linkedin_data_detail'),
-
-    # PATH TWITTER, TIKTOK, FACEBOOK (jangan dihapus)
-#     path('social-medial/twitter/<str:ue1>/', name='twitter_data_detail'),
-#     path('social-medial/tiktok/<str:ue1>', name='tiktok_data_detail'),
-#     path('social-medial/facebook/<str:ue1>/', name='facebook_data_detail'),
+    path('download_excel/<str:ue1>/', views.download_excel, name='download_excel'),
 ]
--- socmed_data/views.py
+++ socmed_data/views.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import pandas as pd
 from django.shortcuts import get_object_or_404, render
 import requests
 from input_agenda_setting.models import InputAgendaSetting
@@ -9,7 +10,7 @@ from requests_oauthlib import OAuth2Session
 from requests_oauthlib.compliance_fixes import linkedin_compliance_fix
 from input_agenda_setting.models import InputAgendaSetting
 from socmed_data.models import LinkedInPost
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from socmed_data.utils import scrape_instagram_data_api
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -17,6 +18,8 @@ from nltk.metrics import jaccard_distance
 from io import BytesIO
 from similarity_checker.views import calculate_similarity
 from django.db.models import Q
+from django.db import transaction
+from openpyxl import Workbook
 
 """
 INSTAGRAM with Web Scrapping
@@ -84,6 +87,70 @@ INSTAGRAM with API Library
 
 #     return render(request, 'instagram/instagram_detail_api.html', context)
 
+# def instagram_detail_api(request, ue1):
+#     agenda_setting = get_object_or_404(InputAgendaSetting)
+#     agenda_start = agenda_setting.agenda_date_time_start
+#     agenda_end = agenda_setting.agenda_date_time_end
+
+#     socmed_data = []
+#     labels_graph = []
+#     data_graph = []
+#     account_data = {}
+
+#     for data in SocialMediaData.objects.filter(ue1=ue1, created_at__range=(agenda_start, agenda_end)):
+#         if data.social_media == 'Instagram':
+#             scraped_data = scrape_instagram_data_api(data.account_url, agenda_start, agenda_end)
+#             captions = scraped_data.get('captions', [])
+#             likes = scraped_data.get('likes', [])
+#             comments = scraped_data.get('comments', [])
+#             viewers = scraped_data.get('viewers', [])
+#             post_urls = scraped_data.get('post_urls', [])
+#             followers = scraped_data.get('followers', [])
+
+#             for post_url, caption, like, comment, viewer in zip(post_urls, captions, likes, comments, viewers):
+#                 # Calculate similarity
+#                 similarity = calculate_similarity(caption, agenda_setting.pesan_kunci, agenda_setting.sub_pesan_kunci)
+#                 data_graph.append(similarity)
+
+#                 updates = {
+#                     'account_url': data.account_url,
+#                     'social_media': data.social_media,
+#                     'ue1': data.ue1,
+#                     'posts': data.posts,
+#                     'followers': followers,
+#                     'post_urls': post_url,
+#                     'captions': caption,
+#                     'viewers': viewer,
+#                     'comments': comment,
+#                     'likes': like,
+#                     'similarity': similarity,
+#                 }
+#                 if data.account_url not in account_data:
+#                     account_data[data.account_url] = []
+#                 account_data[data.account_url].append(updates)
+
+#                 # Update the queryset with OR conditions for post_url and ue1
+#                 SocialMediaData.objects.filter(Q(ue1=data.ue1) & Q(post_urls=post_url)).update(**updates)
+
+#     # Calculate average similarity
+#     if len(data_graph) > 0:
+#         average_similarity_graph = sum(data_graph) / len(data_graph) * 100
+#     else:
+#         average_similarity_graph = 0.0
+
+#     context = {
+#         'ue1': ue1,
+#         'agenda_start': agenda_start,
+#         'agenda_end': agenda_end,
+#         'socmed_data': socmed_data,
+#         'average_similarity': average_similarity_graph,
+#         'account_data': account_data,
+#         'labels_json': json.dumps(labels_graph),
+#         'data_json': json.dumps(data_graph),
+#     }
+
+#     return render(request, 'instagram/instagram_detail_api.html', context)
+
 def instagram_detail_api(request, ue1):
     agenda_setting = get_object_or_404(InputAgendaSetting)
     agenda_start = agenda_setting.agenda_date_time_start
@@ -94,64 +161,102 @@ def instagram_detail_api(request, ue1):
     data_graph = []
     account_data = {}
 
-    for data in SocialMediaData.objects.filter(ue1=ue1, created_at__range=(agenda_start, agenda_end)):
-        if data.social_media == 'Instagram':
-            scraped_data = scrape_instagram_data_api(data.account_url, agenda_start, agenda_end)
-            captions = scraped_data.get('captions', [])
-            likes = scraped_data.get('likes', [])
-            comments = scraped_data.get('comments', [])
-            viewers = scraped_data.get('viewers', [])
-            post_urls = scraped_data.get('post_urls', [])
-            followers = scraped_data.get('followers', [])
+    # Get all unique account URLs for the specified UE1
+    account_urls = SocialMediaData.objects.filter(ue1=ue1).values_list('account_url', flat=True).distinct()
 
-            for post_url, caption, like, comment, viewer in zip(post_urls, captions, likes, comments, viewers):
-                # Convert the captions into a list
-                caption_list = [caption.strip() for caption in caption.split('\n') if caption.strip()]
+    for account_url in account_urls:
+        scraped_data = scrape_instagram_data_api(account_url, agenda_start, agenda_end)
+        captions = scraped_data.get('captions', [])
+        likes = scraped_data.get('likes', [])
+        comments = scraped_data.get('comments', [])
+        viewers = scraped_data.get('viewers', [])
+        post_urls = scraped_data.get('post_urls', [])
+        followers = scraped_data.get('followers', [])
+        posts = scraped_data.get('posts', 0)
 
-                for caption_text in caption_list:
-                    similarity = calculate_similarity(caption_text, agenda_setting.pesan_kunci, agenda_setting.sub_pesan_kunci)
-                    data_graph.append(similarity)
+        account_info = {
+            'account_url': account_url,
+            'followers': followers,
+            'posts': posts,
+            'account_data': [],
+        }
 
-                    updates = {
-                        'account_url': data.account_url,
-                        'social_media': data.social_media,
-                        'ue1': data.ue1,
-                        'posts': data.posts,
-                        'followers': followers,
-                        'post_urls': post_url,
-                        'captions': caption_list,
-                        'viewers': viewer,
-                        'comments': comment,
-                        'likes': like,
-                        'similarity': similarity,
-                    }
-                    if data.account_url not in account_data:
-                        account_data[data.account_url] = []
-                    account_data[data.account_url].append(updates)
-                
-                # Update the queryset with OR conditions for post_url and ue1
-                SocialMediaData.objects.filter(Q(ue1=data.ue1) & Q(post_urls=post_url)).update(**updates)
-                    
+        account_info['account_data'] = [
+            {
+                'post_urls': post_url,
+                'captions': caption,
+                'likes': like,
+                'comments': comment,
+                'viewers': viewer,
+                'similarity': calculate_similarity(caption, agenda_setting.pesan_kunci, agenda_setting.sub_pesan_kunci),
+            }
+            for post_url, caption, like, comment, viewer
+            in zip(post_urls, captions, likes, comments, viewers)
+        ]
+
+        # Calculate and append similarity values to data_graph
+        similarity_values = [
+            calculate_similarity(caption, agenda_setting.pesan_kunci, agenda_setting.sub_pesan_kunci)
+            for caption in captions
+        ]
+        data_graph.extend(similarity_values)
+
+        account_data[account_url] = account_info
+
     # Calculate average similarity after the loop
     if len(data_graph) > 0:
         average_similarity_graph = sum(data_graph) / len(data_graph) * 100
-        print("avg sim graph: ", average_similarity_graph)
     else:
         average_similarity_graph = 0.0
-        print("avg is 0.0")
 
     context = {
         'ue1': ue1,
         'agenda_start': agenda_start,
         'agenda_end': agenda_end,
         'socmed_data': socmed_data,
-        'average_similarity': average_similarity_graph,
         'account_data': account_data,
+        'average_similarity': average_similarity_graph,
         'labels_json': json.dumps(labels_graph),
         'data_json': json.dumps(data_graph),
     }
 
     return render(request, 'instagram/instagram_detail_api.html', context)
+
+def download_excel(request):
+    # Extract the data from the POST request's body
+    post_data = request.POST.get('data', '')
+
+    # Convert the JSON data to a list of dictionaries
+    data_list = json.loads(post_data)
+
+    # Create a DataFrame from the list of dictionaries
+    df = pd.DataFrame(data_list)
+
+    # Create an in-memory Excel file using Pandas
+    excel_file = BytesIO()
+    with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
+        df.to_excel(writer, sheet_name='Sheet1', index=False)
+        workbook = writer.book
+        worksheet = writer.sheets['Sheet1']
+
+        # Set the column widths based on content
+        for idx, column in enumerate(df):
+            series = df[column]
+            max_len = max(
+                series.astype(str).apply(len).max(),
+                len(str(column))
+            )
+            col_width = max_len + 2  # padding
+            worksheet.set_column(idx, idx, col_width)
+
+    # Prepare the response
+    response = HttpResponse(
+        excel_file.getvalue(),
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    response['Content-Disposition'] = 'attachment; filename=social_media_data.xlsx'
+
+    return response
 
 def test(request):
     return render(request, 'test.html')
@@ -264,9 +369,9 @@ def fetch_linkedin_data_oauth2(request):
 
     return render(request, "linkedin/authorization_linkedin.html", {'authorization_url': authorization_url})
 
-"""
-LINKEDIN without OAuth2
-"""
+# """
+# LINKEDIN without OAuth2
+# """
 def fetch_linkedin_data(request):
     agenda_setting = InputAgendaSetting.objects.get(id=2)
 
